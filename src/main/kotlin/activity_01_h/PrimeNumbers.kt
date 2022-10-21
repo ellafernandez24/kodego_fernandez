9fc0d05b79e37b7c4ef1427cb5ca2c7666baf2be
Ma. Mickaella Rica C. Fernandez <ellacabrera1205@gmail.com>
2022-10-21 12:05:18 +0800

    Added PrimeNumbers.kt in package activity_01_h
    Activity 01 - H

---
+++ src/main/kotlin/activity_01_h/PrimeNumbers.kt
@@ -0,0 +1,10 @@
+//Create an application that will accept 2 Integers.
+//It will identify the lower value and bigger value of the two inputs.
+//Then, your application will print all the prime numbers starting from the lower number up to the largest number.
+package activity_01_h
+import mu.KotlinLogging
+private val logger = KotlinLogging.logger{}
+
+fun main(){
+
+}

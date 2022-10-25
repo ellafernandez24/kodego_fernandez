845c9511492ff5d87cb4798db6a6f9c72f261f9d
Ma. Mickaella Rica C. Fernandez <ellacabrera1205@gmail.com>
2022-10-25 15:50:39 +0800

    Added SchoolLibrary.kt on activity_02_b
    Activity 02 - B

---
+++ src/main/kotlin/activity_02_b/SchoolLibrary.kt
@@ -0,0 +1,19 @@
+//Implement Activity 01 -  B using data structure.
+package activity_02_b
+import java.time.format.DateTimeFormatter
+
+fun main(){
+
+    var callNumber: ArrayList<String> = ArrayList()
+    var Books: ArrayList<String> = ArrayList()
+    var ReferenceBooks: ArrayList<String> = ArrayList()
+    var childrenBooks: ArrayList<String> = ArrayList()
+    var dvd: ArrayList<String> = ArrayList()
+    var periodicalJournal: ArrayList<String> = ArrayList()
+    var periodicalMagazine: ArrayList<String> = ArrayList()
+    var periodicalNewspaper: ArrayList<String> = ArrayList()
+    var specialCollection: ArrayList<String> = ArrayList()
+    var dateBorrowed = DateTimeFormatter.ofPattern("dd-MMMM-yyyy")
+    var dateReturned = DateTimeFormatter.ofPattern("dd-MMMM-yyyy")
+}
+

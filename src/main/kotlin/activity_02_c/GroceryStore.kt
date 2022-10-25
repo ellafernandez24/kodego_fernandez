857d587d4b354e957c66c205783ae31bed8f6a17
Ma. Mickaella Rica C. Fernandez <ellacabrera1205@gmail.com>
2022-10-25 15:49:17 +0800

    Added GroceryStore.kt on activity_02_c
    Activity 02 - C

---
+++ src/main/kotlin/activity_02_c/GroceryStore.kt
@@ -0,0 +1,17 @@
+//Implement Activity 01 - C using data structure.
+package activity_02_c
+
+fun main () {
+
+    var cannedDryGoods:ArrayList<String> = ArrayList()
+    var bakingSupplies:ArrayList<String> = ArrayList()
+    var herbsAndSpices:ArrayList<String> = ArrayList()
+    var fruitsAndVegetables:ArrayList<String> = ArrayList()
+    var dairyProducts:ArrayList<String> = ArrayList()
+    var meatProducts:ArrayList<String> = ArrayList()
+    var seaFoodProducts:ArrayList<String> = ArrayList()
+    var householdProducts:ArrayList<String> = ArrayList()
+    var personalProducts:ArrayList<String> = ArrayList()
+    var miscellaneousProducts:ArrayList<String> = ArrayList()
+}
+
